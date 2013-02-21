d940a28d38b3578eb5fc053d07574cdcd3386a9b
SergeyBerdnikovich <atom@tut.by>
2013-02-21 11:01:11 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sneakers::Application.config.secret_token = 'd890894e99c64e075b279acae69bef1d7039689ea12181e5915f036aefc5f771a264439adac3d07d3620dbab5dcf41e17198b0e45204f6a420308f3fc492d8fd'
