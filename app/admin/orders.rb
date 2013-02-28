27d29d856d393dc8b6176ca61852ac0943c25190
SergeyBerdnikovich <atom@tut.by>
2013-02-28 15:36:16 +0300

    updated admin-orders form

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Order do
-  
+  form do |f|
+    f.inputs 'User' do
+      f.select("user_id", User.all.collect {|p| [ p.email, p.id ] })
+    end
+    f.inputs 'Order' do
+      f.input :city
+      f.input :product
+      f.input :size
+      f.input :name
+      f.input :paypal_customer_token
+      f.input :paypal_recurring_profile_token
+    end
+    f.actions
+  end
 end
