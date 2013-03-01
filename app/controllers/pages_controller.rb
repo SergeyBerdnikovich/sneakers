6404e994e1954716177fc866555fe4000590a456
SergeyBerdnikovich <atom@tut.by>
2013-03-01 12:14:48 +0300

    updated pages/welcome added border to latest products

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 class PagesController < ApplicationController
   def welcome
     @gallery_for_slider = Gallery.for_slider
-    @last_products = Product.limit(6)
+    @latest_products = Product.all
 
     gb = Gibbon.new("ba8bef03ff6ec6f2659a03ff0e92eea5-us6")
     @lists = gb.lists({:start => 0, :limit=> 100})["data"]
