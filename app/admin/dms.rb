431e68e8dae917fe0f5c33431d17d9cf0005b3d4
SergeyBerdnikovich <atom@tut.by>
2013-03-07 18:23:07 +0300

    updated admin-dms index

--- app/admin/dms.rb
+++ app/admin/dms.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Dms do
-  
+  index do
+    column :id
+    column :twitter_name do |dms|
+      dms.twitter_account.name if dms.twitter_account
+    end
+    column :text
+    column :received
+    column :sender
+    column :created_at
+    column :updated_at
+
+    default_actions
+  end
 end
--- app/models/dms.rb
+++ app/models/dms.rb
@@ -1,3 +1,5 @@
 class Dms < ActiveRecord::Base
-  attr_accessible :received, :text, :twitter_account_id
+  attr_accessible :received, :text, :twitter_account_id, :sender
+
+  belongs_to :twitter_account
 end
